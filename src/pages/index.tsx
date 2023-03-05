f80eba78932bfbe83368961140837d43aeb3f770
Tsuchida Misaki <misaki11290122@icloud.com>
2023-03-05 22:14:09 +0900

    a

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,3 @@
-export default function Home() {
-  return  <h1>iGEM Kyoto</h1>
+export default function Home(): JSX.Element {
+  return  <h1>iGEM Kyoto<br></br>iGEM × Kyoto = ∞<br></br>We are iGEM Kyoto, Kyoto University team competing in the international Genetically Engineered Machine competition, in short iGEM.<br></br>Challenges in the 15th year</h1>
 }
