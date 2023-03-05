f7055c62d669b259d706cfbb0fe35184d2292bd7
Tsuchida Misaki <misaki11290122@icloud.com>
2023-03-05 22:29:18 +0900

    s

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,8 @@
-export default function Home(): JSX.Element {
-  return  <h1>iGEM Kyoto<br></br>iGEM × Kyoto = ∞<br></br>We are iGEM Kyoto, Kyoto University team competing in the international Genetically Engineered Machine competition, in short iGEM.<br></br>Challenges in the 15th year</h1>
+export default function Home() {
+  return (
+    <> 
+      <h1>iGEM Kyoto<br></br>iGEM × Kyoto = ∞<br></br>We are iGEM Kyoto, Kyoto University team competing in the international Genetically Engineered Machine competition, in short iGEM.<br></br>Challenges in the 15th year</h1>
+   
+    </>
+  )
 }
