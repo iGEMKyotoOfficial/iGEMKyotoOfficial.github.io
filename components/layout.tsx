e8c1027dd9b733caddcd4e4565afebbe8d73d804
Tsuchida Misaki <misaki11290122@icloud.com>
2023-03-12 20:20:02 +0900

    a

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,8 @@
 import Header from 'components/header'
 import Footer from 'components/footer'
 
-export default function Layout({ children }){
+export default function Layout({ children }:
+  {children:any}){
   return (
     <>
       <Header />
---
+++ src/pages/api/_app.tsx
@@ -0,0 +1,12 @@
+import Layout from 'components/layout'
+
+function MyApp({ Component, pageProps }:
+    {Component:any, pageProps:any}){
+    return(
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    )
+}
+
+export default MyApp
