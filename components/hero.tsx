946321775bfeb91fdcb3084af9b5cd819e2ac010
Tsuchida Misaki <misaki11290122@icloud.com>
2023-03-13 20:22:13 +0900

    j

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,10 +1,14 @@
+import styles from 'styles/hero.module.css'
+
 export default function Hero({ title, subtitle, imageOn = false }:
   {title:any; subtitle:any; imageOn:any;})
   {
     return (
         <div>
-          <h1>{title}</h1>
-          <p>{subtitle}</p>
+          <div className={styles.text}>
+            <h1 className={styles.title}>{title}</h1>
+            <p className={styles.subtitle}>{subtitle}</p>
+          </div>
           {imageOn && <figure> [画像] </figure>}
         </div>
     )
