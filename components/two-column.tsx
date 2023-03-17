d4e20ef7c85c76d93ea947138c2a63ece446099f
Tsuchida Misaki <misaki11290122@icloud.com>
2023-03-17 20:29:16 +0900

    e

--- components/two-column.tsx
+++ components/two-column.tsx
@@ -1,24 +1,24 @@
 import styles from 'src/styles/two-column.module.css'
 
-export function TwoColumn({ children }) {
+export function TwoColumn({ children }:{children:any}) {
     return (
-        <div className={styles.flexContainer}
+        <div className={styles.flexContainer}>
             {children}
         </div>
     )
 }
 
-export function TwoColumnMain({ children }) {
+export function TwoColumnMain({ children }:{children:any}) {
     return (
-        <div className={styles.main}
+        <div className={styles.main}>
             {children}
         </div>
     )
 }
 
-export function TwoColumnSidebar({ children }) {
+export function TwoColumnSidebar({ children }:{children:any}) {
     return (
-        <div className={styles.sidebar}
+        <div className={styles.sidebar}>
             {children}
         </div>
     )
