c2d78687a346b9c537bebfb414a571bce8146442
Tsuchida Misaki <misaki11290122@icloud.com>
2023-03-17 10:57:40 +0900

    g

---
+++ components/contact.tsx
@@ -0,0 +1,10 @@
+import styles from 'src/styles/contact.module.css'
+
+export default function Contact() {
+    return (
+        <div className={styles.stack}>
+            <h3 className={styles.heading}>Contact</h3>
+            <address>igemkyoto2023@gmail.com</address>
+        </div>
+    )
+}
--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 export default function Footer(){
     return (
         <footer className={styles.wrapper}>
-            < Container children={undefined} large={undefined} >
+            < Container >
                 <div className={styles.flexContainer}>
                     <Link href="/blog" legacyBehavior>
                         <a className="home">iGEM Kyoto</a>
