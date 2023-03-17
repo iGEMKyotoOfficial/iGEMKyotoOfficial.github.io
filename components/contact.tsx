93fbc701e1f62155b16f2859950a8d1cc6650cea
Tsuchida Misaki <misaki11290122@icloud.com>
2023-03-17 17:44:55 +0900

    k

--- components/contact.tsx
+++ components/contact.tsx
@@ -4,7 +4,7 @@ export default function Contact() {
     return (
         <div className={styles.stack}>
             <h3 className={styles.heading}>Contact</h3>
-            <address>igemkyoto2023<span className="no-spam">loshfugcygbfjbf</span>@gmail.com</address>
+            <address>メールアドレス</address>
         </div>
     )
 }
