0e04985ebe7e122e0d608ccf97df5e1c721b108f
Capobmb <82255007+Capobmb@users.noreply.github.com>
2023-03-18 19:38:48 +0900

    added index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Hero from 'components/hero'
+
+export default function index() {
+  return (
+    <>
+      <Hero
+        title="iGEM Kyoto"
+        subtitle="iGEM × Kyoto = ∞"
+        imageOn
+      />
+      <p>
+        コンヌツKyotoでおま
+      </p>
+    </>
+  )
+}
