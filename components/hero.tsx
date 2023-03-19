f4e2b1520ca44327bda2e85c92934f9d9d95562d
Tsuchida Misaki <misaki11290122@icloud.com>
2023-03-19 19:32:38 +0900

    彼女好きすぎて頭がおかしくなり精神が崩壊するかもしれません

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,29 +1,23 @@
 import styles from 'src/styles/hero.module.css'
 import Image from 'next/image'
-import cube from 'images/dna-3539309.jpg'
+import cube from 'images/fushimi-inari-shrine-1886975.jpg'
 import Container from 'components/container'
 
-export default function Hero({ title, subtitle, imageOn = false }:
+export default function Hero({ title, subtitle, imageOn = true }:
   { title: any; subtitle: any; imageOn: any; }) {
   return (
     <div className={styles.flexContainer}>
-      <Container large={false}>
-        <div className={styles.text} style={{backgroundImage: `url(../images/dna-3539309.jpg)`}}>
+      <Container large={true}>
+        <div className={styles.text} >
           <h1 className={styles.title}>{title}</h1>
           <p className={styles.subtitle}>{subtitle}</p>
         </div>
-      </Container>
-      {/* <Container large={true}>
         {imageOn && (
-          <figure className={styles.image}>
-            <Image
-              src={cube}
-              alt=""
-              layout="responsive"
-            />
+          <figure>
+            <Image src={cube} alt="" layout="responsive" />
           </figure>
         )}
-      </Container> */}
+      </Container>
     </div>
   )
 }
