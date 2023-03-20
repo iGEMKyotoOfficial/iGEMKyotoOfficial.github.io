48595ba8de4ed526d678bd49841dcb7a61734f67
Tsuchida Misaki <misaki11290122@icloud.com>
2023-03-20 13:28:16 +0900

    恋人ってまだそれと同時に好きな人でもあるから気遣ってしまうところある

--- components/social.tsx
+++ components/social.tsx
@@ -3,7 +3,32 @@ import styles from 'src/styles/social.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
     faTwitter,
-    faFacebookF,
     faInstagram,
+    faFacebookF,
 } from '@fortawesome/free-brands-svg-icons'
+
+export default function Social() {
+    return (
+        <ul className={styles.list}>
+            <li>
+               <a href="https://twitter.com/iGEMkyoto">
+                    <FontAwesomeIcon icon={faTwitter} />
+                    <span className="sr-only">Twitter</span>
+               </a>
+            </li>
+            <li>
+                <a href="https://www.instagram.com/igemkyoto/">
+                    <FontAwesomeIcon icon={faInstagram} />
+                    <span className="sr-only">Instagram</span>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.facebook.com/IgemKyoto/">
+                    <FontAwesomeIcon icon={faFacebookF} />
+                    <span className="sr-only">Facebook</span>
+                </a>
+            </li>
+        </ul>
+    )
+}
 
