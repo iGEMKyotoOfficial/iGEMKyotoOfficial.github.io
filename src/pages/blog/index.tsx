20b773488019f6d58b599e1b2b2d4b0064be95b1
Tsuchida Misaki <misaki11290122@icloud.com>
2023-03-28 21:01:35 +0900

    いー感じい

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,15 +1,28 @@
 import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
+import Image from 'next/image'
+import fushimi from 'images/fushimi-inari-shrine-1886975.jpg'
 
 export default function Home() {
   return (
     <Container large>
-      <Meta pageTitle="Home" pageDesc=""/>
-        <Hero
-          title="iGEM Kyoto"
-          subtitle="iGEM × Kyoto = ∞"
+      <Meta pageTitle="Home" pageDesc="" />
+      <Hero
+        title="iGEM Kyoto"
+        subtitle="iGEM × Kyoto = ∞"
+      />
+
+      <figure>
+        <Image
+          src={fushimi}
+          alt=""
+          layout="responsive"
+          sizes="100vw"
+          priority
         />
+      </figure>
+
     </Container>
   )
 }
