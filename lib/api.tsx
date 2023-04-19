e85a0f75524da16172fd7e5d68e6397d8767b3d7
Tsuchida Misaki <misaki11290122@icloud.com>
2023-04-19 22:50:51 +0900

    s

--- lib/api.tsx
+++ lib/api.tsx
@@ -4,3 +4,16 @@ export const client = createClient({
     serviceDomain: process.env.SERVICE_DOMAIN,
     apiKey: process.env.API_KEY,
 })
+
+export async function from getPostBySlug(slug) ;{
+    try {
+        const post = await client.get({
+            endpoint: 'igemkyoto',
+            queries: { filters: 'slug[equels]${slug}' },
+        })
+        return post.contents[0]
+    } catch (err) {
+        console.log('~~ getPostBySlug ~~')
+        console.log(err)
+    }
+}
