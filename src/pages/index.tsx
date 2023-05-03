13541b6c2de69e721f4e808b037734bea16918f3
Tsuchida Misaki <misaki11290122@icloud.com>
2023-05-03 18:25:48 +0900

    pagenation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,12 @@ export default function Home() {
           title="iGEM Kyoto"
           subtitle="私たちiGEM Kyotoは合成生物学の世界大会iGEMに向けて活動している京大チームです"
         />
-        <a class="twitter-timeline" data-width="500" data-height="500" data-theme="dark" href="https://twitter.com/iGEMkyoto?ref_src=twsrc%5Etfw">Tweets by iGEMkyoto</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        <a className="twitter-timeline" data-width="700" data-height="500" data-theme="dark" href="https://twitter.com/iGEMkyoto?ref_src=twsrc%5Etfw">
+          Tweets by iGEMkyoto
+        </a> 
+        <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8">
+        </script>
+        <div><br /></div>
     </Contain>
   )
 }
--- src/pages/member.tsx
+++ src/pages/member.tsx
@@ -66,6 +66,23 @@ export default function About() {
         </Link>
       </div>
 
+      <div className="pagination-wrapper">
+        <div className="pagination">
+          <a className="prev page-numbers" href="javascript:;">prev</a>
+          <span aria-current="page" className="page-numbers current">1</span>
+          <a className="page-numbers" href="javascript:;">2</a>
+          <a className="page-numbers" href="javascript:;">3</a>
+          <a className="page-numbers" href="javascript:;">4</a>
+          <a className="page-numbers" href="javascript:;">5</a>
+          <a className="page-numbers" href="javascript:;">6</a>
+          <a className="page-numbers" href="javascript:;">7</a>
+          <a className="page-numbers" href="javascript:;">8</a>
+          <a className="page-numbers" href="javascript:;">9</a>
+          <a className="page-numbers" href="javascript:;">10</a>
+          <a className="next page-numbers" href="javascript:;">next</a>
+        </div>
+      </div>
+
       <br />
       <Frame subtitle="石橋 凌平 (Ryohei ISHIBASHI)" src={ishibashi}>
         Sophomore of the Faculty of Science, Kyoto University <br />
@@ -135,7 +152,7 @@ export default function About() {
         Role: Paper Search and Wet Experiment <br />
         Hobby: 執筆 <br />
         パラジウム触媒万能
-      </Frame>  
+      </Frame>
     </Contain>
   )
 }
