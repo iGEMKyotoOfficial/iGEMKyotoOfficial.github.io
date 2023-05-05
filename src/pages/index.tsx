cdb173a9abbb45949fabe19799b362239f778355
Tsuchida Misaki <misaki11290122@icloud.com>
2023-05-05 17:03:46 +0900

    home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,18 +9,18 @@ export default function Home() {
     <Contain large>
       <Meta />
 
-      <svg viewBox="0 0 800 600" >
+      <svg viewBox="0 0 800 400" >
         <symbol id="s-text">
           <text text-anchor="middle"
             x="50%"
-            y="25%"
+            y="40%"
             className="text--line"
           >
             iGEM
           </text>
           <text text-anchor="middle"
             x="50%"
-            y="55%"
+            y="80%"
             className="text--line"
           >
             Kyoto
@@ -41,7 +41,12 @@ export default function Home() {
         </g>
       </svg>
 
-      <Link className="twitter-timeline" data-width="700" data-height="700" data-theme="light" href="https://twitter.com/iGEMkyoto?ref_src=twsrc%5Etfw">
+      <Link className="twitter-timeline" 
+      data-width="700" 
+      data-height="700" 
+      data-theme="light" 
+      href="https://twitter.com/iGEMkyoto?ref_src=twsrc%5Etfw"
+      >
         Tweets by iGEMkyoto
       </Link>
       <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8">
