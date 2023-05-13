ede3e45d8e37fab6f6f92e7c1f08e0579a418cc0
Tsuchida Misaki <misaki11290122@icloud.com>
2023-05-13 16:48:56 +0900

    hamburger

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,37 +1,61 @@
 import {useState} from 'react'
 import Link from 'next/link'
 import styles from 'src/styles/nav.module.css'
+import { faStackOverflow } from '@fortawesome/free-brands-svg-icons'
 
 export default function Nav(){
-    const [navIsOpen, setnavUsOpen] = useState(false)
+    const [navIsOpen, setNavIsOpen] = useState(false)
+
+    const toggleNav = () => {
+        setNavIsOpen((prev) => !(prev))
+    }
+
+    const closeNav = () => {
+        setNavIsOpen(false)
+    }
 
     return(
         <nav className={navIsOpen ? styles.open : styles.close}>
-            <button className={styles.button}>MENU</button>
+            {navIsOpen && (
+                <style jsx global>{`
+                  @media (max-width: 767px) {
+                    body {
+                        overflow: hidden;
+                        position: fixed;
+                        width: 100%;
+                    }
+                  }
+                `}</style>
+            )}
+
+            <button className={styles.btn} onClick={toggleNav}>
+                <span className={styles.bar}></span>
+                <span className="sr-only">MENU</span>
+            </button>
             <ul className={styles.list}>
                 <li>
                     <Link href="/" legacyBehavior>
-                            <a className="home">Home</a>
+                            <a onClick={closeNav} className="home">Home</a>
                     </Link>
                 </li>
                 <li>
                     <Link href="/about" legacyBehavior>
-                        <a className="about">About</a>
+                        <a onClick={closeNav} className="about">About</a>
                     </Link>
                 </li>
                 <li>
                     <Link href="/member" legacyBehavior>
-                        <a className="member">メンバー</a>
+                        <a onClick={closeNav} className="member">メンバー</a>
                     </Link>
                 </li>
                 <li>
                     <Link href="/finance" legacyBehavior>
-                        <a className="finance">協賛・寄付</a>
+                        <a onClick={closeNav} className="finance">協賛・寄付</a>
                     </Link>
                 </li>
                 <li>
                     <Link href="/contact" legacyBehavior>
-                        <a className="contact">お問い合わせ</a>
+                        <a onClick={closeNav} className="contact">お問い合わせ</a>
                     </Link>
                 </li>
             </ul>
