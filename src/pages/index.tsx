5d6d1e72cf7da6db72f95bf729c20711ba5d051e
Tsuchida Misaki <misaki11290122@icloud.com>
2023-05-14 19:19:57 +0900

    sitemap

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,10 @@ import 'bootstrap/dist/css/bootstrap.css'
 import Link from 'next/link'
 import sakura from 'images/sakura.jpg'
 import Image from 'next/image'
+import dna from 'images/dna.jpg'
+import fushimi from 'images/fushimi.jpg'
+import ume from 'images/ume.jpg'
+import bamboo from 'images/bamboo.jpeg'
 
 export default function Home() {
 
@@ -12,16 +16,85 @@ export default function Home() {
     <Contain large>
       <Meta />
       <div className="anime_wrap kenburns-top-right">
-          <Image className="sakura"
-            src={sakura}
-            alt=""
-            layout="responsive"
-            sizes='100vw'
-            priority
-          />
+        <Image className="sakura"
+          src={sakura}
+          alt=""
+          layout="responsive"
+          sizes='100vw'
+          priority
+        />
         <p className='tracking-in-contract'>iGEM Kyoto</p>
       </div>
 
+      <div className="cp_card05" >
+        <Image className="dna"
+          src={dna}
+          alt=""
+          layout="responsive"
+          sizes='100vw'
+          priority
+        />
+        <div className="description">
+          <div className="content">
+            <h1><a href="/about">About</a></h1>
+            <p className="text">iGEM Kyoto、iGEM、合成生物学についてご説明します</p>
+            <a href="/about" className="button">PUSH</a>
+          </div>
+        </div>
+      </div>
+
+      <div className="cp_card05" >
+        <Image className="ume"
+          src={ume}
+          alt=""
+          layout="responsive"
+          sizes='100vw'
+          priority
+        />
+        <div className="description">
+          <div className="content">
+            <h1><a href="/member">Member</a></h1>
+            <p className="text">歴代メンバーをご紹介します（iGEM Jamboreeに出場した年は、Wikiもご覧いただけます）</p>
+            <a href="/member" className="button">PUSH</a>
+          </div>
+        </div>
+      </div>
+
+      <div className="cp_card05" >
+        <Image className="fushimi"
+          src={fushimi}
+          alt=""
+          layout="responsive"
+          sizes='100vw'
+          priority
+        />
+        <div className="description">
+          <div className="content">
+            <h1><a href="/finance">支援のお願い</a></h1>
+            <p className="text">私たちを応援してくださる企業・個人の方を募集しております</p>
+            <a href="/finance" className="button">PUSH</a>
+          </div>
+        </div>
+      </div>
+
+      <div className="cp_card05" >
+        <Image className="bamboo"
+          src={bamboo}
+          alt=""
+          layout="responsive"
+          sizes='100vw'
+          priority
+        />
+        <div className="description">
+          <div className="content">
+            <h1><a href="/contact">お問い合わせ</a></h1>
+            <p className="text">依頼や質問をご希望の方はこちらからお問い合わせください</p>
+            <a href="/contact" className="button">PUSH</a>
+          </div>
+        </div>
+      </div>
+
+      <br />
       <Link className="twitter-timeline"
         data-width="700"
         data-height="700"
