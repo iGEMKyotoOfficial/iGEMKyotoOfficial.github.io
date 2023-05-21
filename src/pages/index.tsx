fb8dd3bec0f919e41ef70aeab1257490411bcbf8
Capobmb <82255007+Capobmb@users.noreply.github.com>
2023-05-21 15:38:19 +0900

    Improved top page animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
           sizes='100vw'
           priority
         />
-        <p className='tracking-in-contract'>iGEM Kyoto</p>
+        <p className='tracking-in-contract kenburns-top-right'>iGEM Kyoto</p>
       </div>
 
       <div className="cp_card05" >
