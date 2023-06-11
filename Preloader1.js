bfeca060b47f0f04d84c6555da491185c36da38f
cara-milk <montanezmyo@gmail.com>
2023-06-11 13:54:11 +0900

    commit

---
+++ Preloader1.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import PulseLoader from "react-loading";
+
+function PreLoader1() {
+  const [data, setData] = useState([]);
+  const [done, setDone] = useState(undefined);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('/about')
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          setData(json);
+          setDone(true);
+        });
+    }, 2000);
+  }, []);
+
+  return (
+    <>
+      {!done ? ( //if NOT done
+        <PulseLoader //built-in react loading thing, change with custom soon
+          color={"#44af6c"}
+        />
+      ) : (//normal post
+        <ul>
+          {data.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
+
+export default PreLoader1;
