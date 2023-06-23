359cbc449e571b67bbd83bb97094e5a304f8065c
cara-milk <montanezmyo@gmail.com>
2023-06-23 20:38:22 +0900

    fixed merge overlaps

--- components/nav.tsx
+++ components/nav.tsx
@@ -2,10 +2,6 @@ import {useState} from 'react'
 import Link from 'next/link'
 import styles from 'src/styles/nav.module.css'
 import { faStackOverflow } from '@fortawesome/free-brands-svg-icons'
-<<<<<<< HEAD
-=======
-
->>>>>>> bd34332 (とりあえずこれをコミット)
 
 export default function Nav(){
     const [navIsOpen, setNavIsOpen] = useState(false)
@@ -14,14 +10,6 @@ export default function Nav(){
         setNavIsOpen((prev) => !(prev))
     }
 
-<<<<<<< HEAD
-    const closeNav = (a: String) => {
-        setNavIsOpen(false)
-        console.log("hi")
-    }
-
-=======
->>>>>>> bd34332 (とりあえずこれをコミット)
     return(
         <nav className={navIsOpen ? styles.open : styles.close}>
             {navIsOpen && (
@@ -49,47 +37,25 @@ export default function Nav(){
                 </li>
                 <li>
                     <Link href="/about" legacyBehavior>
-<<<<<<< HEAD
-                        <a onClick={() => closeNav("/about")} className="about">About</a>
-=======
-                        <a className="about">About</a>
->>>>>>> bd34332 (とりあえずこれをコミット)
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/member" legacyBehavior>
-<<<<<<< HEAD
-                        <a onClick={() => closeNav("/member")} className="member">メンバー</a>
-=======
-                        <a className="member">メンバー</a>
->>>>>>> bd34332 (とりあえずこれをコミット)
                     </Link>
                 </li>
                 <li>
                     <Link href="/activities" legacyBehavior>
-<<<<<<< HEAD
                         <a onClick={() => closeNav("/activities")} className="activities">活動紹介</a>
-=======
                         <a className="activities">活動紹介</a>
->>>>>>> bd34332 (とりあえずこれをコミット)
                     </Link>
                 </li>
                 <li>
                     <Link href="/finance" legacyBehavior>
-<<<<<<< HEAD
                         <a onClick={() => closeNav("/finance")} className="finance">協賛・寄付</a>
-=======
                         <a className="finance">協賛・寄付</a>
->>>>>>> bd34332 (とりあえずこれをコミット)
                     </Link>
                 </li>
                 <li>
                     <Link href="/contact" legacyBehavior>
-<<<<<<< HEAD
                         <a onClick={() => closeNav("/contact")} className="contact">お問い合わせ</a>
-=======
                         <a className="contact">お問い合わせ</a>
->>>>>>> bd34332 (とりあえずこれをコミット)
+
                     </Link>
                 </li>
             </ul>
