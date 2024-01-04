cd04b29d2811e5b1a83064169c6cde72b502cb35
Capobmb <82255007+Capobmb@users.noreply.github.com>
2024-01-04 19:56:06 +0900

    fix navbar

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,3 @@
-import Contain from "components/container";
 import Logo from "components/logo";
 import Nav from "components/nav";
 import styles from "src/styles/header.module.css";
@@ -6,12 +5,13 @@ import styles from "src/styles/header.module.css";
 export default function Header() {
   return (
     <header>
-      <Contain large>
-        <div className={styles.flexContain}>
-          <Logo />
-          <Nav />
-        </div>
-      </Contain>
+      <div
+        className={styles.flexContain}
+        style={{ position: "fixed", top: 0, right: 0, width: "100%" }}
+      >
+        <Logo />
+        <Nav />
+      </div>
     </header>
   );
 }
