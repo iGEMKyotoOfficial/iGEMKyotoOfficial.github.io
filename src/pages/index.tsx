2d6e93c38c31ddb2dcfff8c45354ba475fd4a21b
Capobmb <82255007+Capobmb@users.noreply.github.com>
2024-01-04 18:12:02 +0900

    change link to card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,22 @@ import Meta from "components/meta";
 import Contain from "components/container";
 import "bootstrap/dist/css/bootstrap.css";
 import Link from "next/link";
-import sakura from "images/others/sakura.jpg";
+import sakura from "public/images/sakura.jpg";
 import Image from "next/image";
 import iGEM from "images/others/iGEM.jpg";
 import fushimi from "images/others/fushimi.jpg";
 import ume from "images/others/ume.jpg";
 import bamboo from "images/others/bamboo.jpeg";
+import discussion from "images/activities/Discussion.jpg";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+import Grid from "@mui/material";
 
 export default function Home() {
+  // CSS property. maxWidth=345, minWidth=275
+  const cardStyle = { maxWidth: 325, minWidth: 275, margin: "10px 1vw" };
   return (
     <Contain large>
       <Meta />
@@ -17,107 +25,89 @@ export default function Home() {
         <div className="top-display sakura-bg">
           <p className="top-title tracking-in-contract">iGEM Kyoto</p>
           <p className="top-description">
-          京都大学の学生チーム <b>iGEM Kyoto</b> は、合成生物学の世界大会 <b>iGEM</b> への参加を通じて、学生が責任と自主性に基づいて自由に研究活動を行うことを目的としています。
-          2008年の発足以来、京都大学の教員の皆様に研究の安全性を監督・指導いただく一方、在野の専門家や各産業分野で従事されている方にアドバイスを受けながら研究活動を行っています。取材活動や調査で発見した実社会の問題に対し、合成生物学の技術を用いた解決策を提示し、実験や数理モデリングを通してその実現性を検証した上、成果をiGEM大会や広報イベントなどで発表しています。
+            京都大学の学生チーム <b>iGEM Kyoto</b> は、合成生物学の世界大会{" "}
+            <b>iGEM</b>{" "}
+            への参加を通じて、学生が責任と自主性に基づいて自由に研究活動を行うことを目的としています。
+            <br />
+            2008年の発足以来、京都大学の教員の皆様に研究の安全性を監督・指導いただく一方、在野の専門家や各産業分野で従事されている方にアドバイスを受けながら研究活動を行っています。取材活動や調査で発見した実社会の問題に対し、合成生物学の技術を用いた解決策を提示し、実験や数理モデリングを通してその実現性を検証した上、成果をiGEM大会や広報イベントなどで発表しています。
           </p>
         </div>
       </div>
 
-      <div className="cp_card05">
-        <Image
-          className="dna"
-          src={iGEM}
-          alt=""
-          layout="responsive"
-          sizes="100vw"
-          priority
-        />
-        <div className="description">
-          <div className="content">
-            <h1>
-              <Link href="/about">About</Link>
-            </h1>
-            <p className="text">
-              iGEM Kyoto、iGEM、合成生物学についてご説明します
-            </p>
-            <Link href="/about" className="button">
-              PUSH
-            </Link>
-          </div>
-        </div>
-      </div>
+      {/* flex container to hold the cards */}
+      <div className="top-card-container">
+        <Card sx={cardStyle}>
+          <CardActionArea LinkComponent={Link} href="/about">
+            <CardMedia>
+              <Image src={iGEM} alt="Picture of the iGEM Awards" height={200} />
+            </CardMedia>
+            <CardContent>
+              <h2>About</h2>
+              <p>まずはiGEM KyotoやiGEM、合成生物学についてご説明します</p>
+            </CardContent>
+          </CardActionArea>
+        </Card>
 
-      <div className="cp_card05">
-        <Image
-          className="ume"
-          src={ume}
-          alt=""
-          layout="responsive"
-          sizes="100vw"
-          priority
-        />
-        <div className="description">
-          <div className="content">
-            <h1>
-              <Link href="/member">Member</Link>
-            </h1>
-            <p className="text">
-              歴代メンバーをご紹介します（iGEM
-              Jamboreeに出場した年は、Wikiもご覧いただけます）
-            </p>
-            <Link href="/member" className="button">
-              PUSH
-            </Link>
-          </div>
-        </div>
-      </div>
+        <Card sx={cardStyle}>
+          <CardActionArea LinkComponent={Link} href="/activities">
+            <CardMedia>
+              <Image
+                src={discussion}
+                alt="Picture of the iGEM Awards"
+                height={200}
+              />
+            </CardMedia>
+            <CardContent>
+              <h2>Activities</h2>
+              <p>iGEM Kyoto の具体的な活動をご紹介します</p>
+            </CardContent>
+          </CardActionArea>
+        </Card>
 
-      <div className="cp_card05">
-        <Image
-          className="fushimi"
-          src={fushimi}
-          alt=""
-          layout="responsive"
-          sizes="100vw"
-          priority
-        />
-        <div className="description">
-          <div className="content">
-            <h1>
-              <Link href="/finance">支援のお願い</Link>
-            </h1>
-            <p className="text">
-              私たちを応援してくださる企業・個人の方を募集しております
-            </p>
-            <Link href="/finance" className="button">
-              PUSH
-            </Link>
-          </div>
-        </div>
-      </div>
+        <Card sx={cardStyle}>
+          <CardActionArea LinkComponent={Link} href="/member">
+            <CardMedia>
+              <Image src={ume} alt="Picture of the iGEM Awards" height={200} />
+            </CardMedia>
+            <CardContent>
+              <h2>Member</h2>
+              <p>歴代メンバーをご紹介します</p>
+            </CardContent>
+          </CardActionArea>
+        </Card>
 
-      <div className="cp_card05">
-        <Image
-          className="bamboo"
-          src={bamboo}
-          alt=""
-          layout="responsive"
-          sizes="100vw"
-          priority
-        />
-        <div className="description">
-          <div className="content">
-            <h1>
-              <Link href="/contact">お問い合わせ</Link>
-            </h1>
-            <p className="text">
-              依頼や質問をご希望の方はこちらからお問い合わせください
-            </p>
-            <Link href="/contact" className="button">
-              PUSH
-            </Link>
-          </div>
-        </div>
+        <Card sx={cardStyle}>
+          <CardActionArea LinkComponent={Link} href="/finance">
+            <CardMedia>
+              <Image
+                src={fushimi}
+                alt="Picture of the iGEM Awards"
+                height={200}
+              />
+            </CardMedia>
+            <CardContent>
+              <h2>Support</h2>
+              <p>私たちを応援してくださる企業・個人の方を募集しております</p>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        {/* お問い合わせ */}
+        <Card sx={cardStyle}>
+          <CardActionArea LinkComponent={Link} href="/contact">
+            <CardMedia>
+              <Image
+                src={bamboo}
+                alt="Picture of the iGEM Awards"
+                height={200}
+              />
+            </CardMedia>
+            <CardContent>
+              <h2>Contact</h2>
+              <p>依頼や質問をご希望の方はこちらからお問い合わせください</p>
+            </CardContent>
+          </CardActionArea>
+        </Card>
       </div>
 
       <br />
