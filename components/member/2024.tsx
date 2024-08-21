ee8b392f06e8250de0576dcfa037082613a9b3f4
jellyfish0198 <mayu.k086214090@gmail.com>
2024-08-21 22:46:33 +0900

    add 2024

--- components/member/2024.tsx
+++ components/member/2024.tsx
@@ -22,42 +22,42 @@ export default function Member() {
 <br />
 <Frame subtitle="岡本 はる (Haru Okamoto)" src={okamoto}>
   Sophomore of the Department of Applied Life Science, the Faculty of Agriculture, Kyoto University <br />
-  Role: <b>Wet Leader</b> <br />
+  Role: Wet Leader <br />
   Hobby: 惰眠、生物採集、買い物、模型ロケット <br />
   Plan, Delay, Cancel, Apologize
 </Frame>
 <br />
 <Frame subtitle="原田 茉優 (Mayu Harada)" src={harada}>
   Sophomore of the Department of Applied Life Science, the Faculty of Agriculture, Kyoto University <br />
-  Role: <b>Project Leader, Human Practice</b> <br />
+  Role: Project Leader, Human Practice <br />
   Hobby: 古刹巡り、ゼンタングル <br />
   永久凍土に移住したい。
 </Frame>
 <br />
 <Frame subtitle="中井 愛(Mana Nakai)" src={nakai}>
   Sophomore of the Department of Bioresource Science, the Faculty of Agriculture, Kyoto University <br />
-  Role: <b>Human Practice Leader</b> <br />
+  Role: Human Practice Leader <br />
   Hobby: お笑い鑑賞 <br />
   愛のある変人になりたい
 </Frame>
 <br />
 <Frame subtitle="橋本 麻利 (Mari Hashimoto)" src={hashimoto}>
   Freshman of the Department of Forest Science, the Faculty of Agriculture, Kyoto University <br />
-  Role: <b> Wiki</b> <br />
+  Role:  Wiki <br />
   Hobby: カエル鑑賞、アラビア語 <br />
   カエルと暮らしたい‼
 </Frame>
 <br />
 <Frame subtitle="矢吹 一真(Kazuma Yabuki)" src={yabuki}>
-  Freshman of the Faculty of Science, Kyoto University
-  Role: <b>Modeling</b> <br />
+  Freshman of the Faculty of Science, Kyoto University <br />
+  Role: Modeling <br />
   Hobby: 将棋、微生物採集、タイピング <br />
   ドベにならないように頑張ります
 </Frame>
 <br />
 <Frame subtitle="岩田 心愛 (Haruchika Iwata)" src={iwata}>
   Freshman of the Department of the Electrical and Electronic Science, the Faculty of Engineering, Kyoto University <br />
-  Role: <b>Hardware</b> <br />
+  Role: Hardware <br />
   Hobby: アニメ鑑賞 <br />
   大学まで片道2時間です
 </Frame>
@@ -65,7 +65,7 @@ export default function Member() {
 
 <Frame subtitle="片岡 茉優 (Mayu Kataoka)" src={kataoka}>
   Freshman of the Department of Industrial Chemistry, the Faculty of Engineering, Kyoto University <br />
-  Role: <b>Wiki</b> <br />
+  Role: Wiki <br />
   Hobby: ピラティス、ヨガ、カフェ巡り <br />
   朝型
 </Frame>
