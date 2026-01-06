c4e21b9b0b9ec117dd2db571a691ae09138d2382
efal-yw <yuki43wakita@icloud.com>
2026-01-06 22:16:49 +0900

    修正

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/homepage",
+  assetPrefix: "/homepage",
   images: {
     unoptimized: true,
   },
